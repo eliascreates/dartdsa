c423b2010c1c1d748439d874650f4c7b4013037d
Elias Kekana <thapeloelias.kekana@gmail.com>
2023-12-05 22:57:41 +0200

    chore: add boilerplate for binary tree

---
+++ lib/binary_tree.dart
@@ -0,0 +1,9 @@
+class BinaryNode<T> {
+  final T value;
+  BinaryNode({required this.value});
+}
+
+List<num> binaryTree({required BinaryNode<num> binaryNode}) {
+  //TODO: write binary tree
+  throw UnimplementedError();
+}
