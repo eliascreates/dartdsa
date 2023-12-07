200f6a821f7dbb712f74c455bd88c13041955926
Elias Kekana <thapeloelias.kekana@gmail.com>
2023-12-07 23:57:25 +0200

    chore: add Binary node

--- lib/binary_tree.dart
+++ lib/binary_tree.dart
@@ -1,7 +1,11 @@
 class BinaryNode<T> {
   final T value;
+  BinaryNode<T>? left;
+  BinaryNode<T>? right;
+
   BinaryNode({required this.value});
 }
+
 
 List<num> binaryTree({required BinaryNode<num> binaryNode}) {
   //TODO: write binary tree
